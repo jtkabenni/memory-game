4a68c7ef940e28f722f7de933ed9517d2c3ddf3e
Jenny Takahara <jenny.takahara@gmail.com>
2023-02-09 23:59:12 -0500

    adding js

---
+++ script.js
@@ -0,0 +1,106 @@
+const gameContainer = document.getElementById("game");
+const bodyContainer = document.querySelector("body");
+const resetButton = document.getElementById("reset");
+const allCards = gameContainer.children;
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+];
+
+let flipped = [];
+let matched = 0;
+let isProcessing = false;
+
+function shuffle(array) {
+  let counter = array.length;
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter);
+    counter--;
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+  return array;
+}
+let shuffledColors = shuffle(COLORS);
+
+function createDivsForColors(colorArray) {
+  for (let [i, color] of colorArray.entries()) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add(color);
+    newDiv.dataset.id = i;
+    newDiv.addEventListener("click", handleCardClick);
+    gameContainer.append(newDiv);
+  }
+}
+
+function flipCard(event) {
+  const { style } = event.target;
+  if (style.backgroundColor == "") {
+    style.backgroundColor = event.target.className;
+  } else {
+    style.backgroundColor = "";
+  }
+}
+
+function addToArray(event) {
+  flipped.push({ card: event.target, id: flipped.length + 1 });
+  console.log(flipped);
+}
+
+function controlMatchLogic() {
+  if (flipped.length === 2) {
+    const firstCard = flipped[0].card;
+    const secondCard = flipped[1].card;
+    if (firstCard.className !== secondCard.className) {
+      isProcessing = true;
+      for (const card of flipped) {
+        setTimeout(function () {
+          card.card.style.backgroundColor = "";
+          isProcessing = false;
+        }, 1000);
+      }
+    } else if (firstCard.dataset.id !== secondCard.dataset.id) {
+      for (const card of flipped) {
+        card.card.removeEventListener("click", handleCardClick);
+      }
+      matched++;
+      isProcessing = false;
+    }
+    flipped = [];
+  }
+}
+
+function youWonNotification() {
+  if (matched == allCards.length / 2) {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = "you win!!!!";
+    newDiv.id = "winner";
+    bodyContainer.append(newDiv);
+  }
+}
+
+function resetGame() {
+  window.location.reload();
+}
+resetButton.addEventListener("click", resetGame);
+
+function handleCardClick(event) {
+  if (isProcessing == false) {
+    flipCard(event);
+    addToArray(event);
+    controlMatchLogic();
+  }
+  youWonNotification();
+}
+
+createDivsForColors(shuffledColors);
